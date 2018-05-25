8da41f042df949bf9af5c54a1cbe86fcfeff0440
Ching Lee <clee@godaddy.com>
2018-05-24 18:27:50 -0700

    short code initial

--- csharp/PhoneNumbers/ShortNumberUtil.cs
+++ csharp/PhoneNumbers/ShortNumberUtil.cs
@@ -15,6 +15,9 @@
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace PhoneNumbers
 {
@@ -28,15 +31,182 @@ namespace PhoneNumbers
     {
         private static PhoneNumberUtil phoneUtil;
 
+        // A mapping from a country calling code to the region codes which denote the region represented
+        // by that country calling code. In the case of multiple regions sharing a calling code, such as
+        // the NANPA regions, the one indicated with "isMainCountryForCode" in the metadata should be
+        // first.
+        private static Dictionary<int, List<string>> countryCallingCodeToRegionCodeMap;
+        internal const string MetaDataFilePrefix = "ShortNumberMetaData.xml";
+
         public ShortNumberUtil()
         {
             phoneUtil = PhoneNumberUtil.GetInstance();
+            countryCallingCodeToRegionCodeMap =
+                BuildMetadataFromXml.GetCountryCodeToRegionCodeMap(MetaDataFilePrefix);
         }
 
         // @VisibleForTesting
         public ShortNumberUtil(PhoneNumberUtil util)
         {
             phoneUtil = util;
+            countryCallingCodeToRegionCodeMap =
+                BuildMetadataFromXml.GetCountryCodeToRegionCodeMap(MetaDataFilePrefix);
+        }
+
+        /**
+        * Returns a list with the region codes that match the specific country calling code. For
+        * non-geographical country calling codes, the region code 001 is returned. Also, in the case
+        * of no region code being found, an empty list is returned.
+        */
+        private List<string> GetRegionCodesForCountryCode(int countryCallingCode)
+        {
+            countryCallingCodeToRegionCodeMap.TryGetValue(countryCallingCode, out List<string> regions);
+            return regions ?? new List<string>();
+        }
+
+        /**
+        * Helper method to check that the country calling code of the number matches the region it's
+        * being dialed from.
+        */
+        private bool RegionDialingFromMatchesNumber(PhoneNumber number,
+            string regionDialingFrom)
+        {
+            List<string> regionCodes = GetRegionCodesForCountryCode(number.CountryCode);
+            return regionCodes.Contains(regionDialingFrom);
+        }
+
+        /**
+        * Check whether a short number is a possible number when dialed from the given region. This
+        * provides a more lenient check than {@link #isValidShortNumberForRegion}.
+        *
+        * @param number the short number to check
+        * @param regionDialingFrom the region from which the number is dialed
+        * @return whether the number is a possible short number
+        */
+        public bool IsPossibleShortNumberForRegion(PhoneNumber number, String regionDialingFrom)
+        {
+            if (!RegionDialingFromMatchesNumber(number, regionDialingFrom))
+            {
+                return false;
+            }
+            PhoneMetadata phoneMetadata =
+                MetadataManager.GetShortNumberMetadataForRegion(regionDialingFrom);
+            if (phoneMetadata == null)
+            {
+                return false;
+            }
+            int numberLength = GetNationalSignificantNumber(number).Length;
+            return phoneMetadata.GeneralDesc.PossibleLengthList.Contains(numberLength);
+        }
+
+        /**
+        * Check whether a short number is a possible number. If a country calling code is shared by
+        * multiple regions, this returns true if it's possible in any of them. This provides a more
+        * lenient check than {@link #isValidShortNumber}. See {@link
+        * #isPossibleShortNumberForRegion(PhoneNumber, String)} for details.
+        *
+        * @param number the short number to check
+        * @return whether the number is a possible short number
+        */
+        public bool IsPossibleShortNumber(PhoneNumber number)
+        {
+            List<string> regionCodes = GetRegionCodesForCountryCode(number.CountryCode);
+            int shortNumberLength = GetNationalSignificantNumber(number).Length;
+            foreach (string region in regionCodes)
+            {
+                PhoneMetadata phoneMetadata = MetadataManager.GetShortNumberMetadataForRegion(region);
+                if (phoneMetadata == null)
+                {
+                    continue;
+                }
+                if (phoneMetadata.GeneralDesc.PossibleLengthList.Contains(shortNumberLength))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+        * Tests whether a short number matches a valid pattern in a region. Note that this doesn't verify
+        * the number is actually in use, which is impossible to tell by just looking at the number
+        * itself.
+        *
+        * @param number the short number for which we want to test the validity
+        * @param regionDialingFrom the region from which the number is dialed
+        * @return whether the short number matches a valid pattern
+        */
+        public bool isValidShortNumberForRegion(PhoneNumber number, string regionDialingFrom)
+        {
+            if (!RegionDialingFromMatchesNumber(number, regionDialingFrom))
+            {
+                return false;
+            }
+            PhoneMetadata phoneMetadata =
+                MetadataManager.GetShortNumberMetadataForRegion(regionDialingFrom);
+            if (phoneMetadata == null)
+            {
+                return false;
+            }
+            string shortNumber = GetNationalSignificantNumber(number);
+            PhoneNumberDesc generalDesc = phoneMetadata.GeneralDesc;
+            if (!MatchesPossibleNumberAndNationalNumber(shortNumber, generalDesc))
+            {
+                return false;
+            }
+            PhoneNumberDesc shortNumberDesc = phoneMetadata.ShortCode;
+            return MatchesPossibleNumberAndNationalNumber(shortNumber, shortNumberDesc);
+        }
+
+        /**
+        * Tests whether a short number matches a valid pattern. If a country calling code is shared by
+        * multiple regions, this returns true if it's valid in any of them. Note that this doesn't verify
+        * the number is actually in use, which is impossible to tell by just looking at the number
+        * itself. See {@link #isValidShortNumberForRegion(PhoneNumber, String)} for details.
+        *
+        * @param number the short number for which we want to test the validity
+        * @return whether the short number matches a valid pattern
+        */
+        public bool IsValidShortNumber(PhoneNumber number)
+        {
+            List<string> regionCodes = GetRegionCodesForCountryCode(number.CountryCode);
+            string regionCode = GetRegionCodeForShortNumberFromRegionList(number, regionCodes);
+            if (regionCodes.Count > 1 && regionCode != null)
+            {
+                // If a matching region had been found for the phone number from among two or more regions,
+                // then we have already implicitly verified its validity for that region.
+                return true;
+            }
+            return isValidShortNumberForRegion(number, regionCode);
+        }
+
+        // Helper method to get the region code for a given phone number, from a list of possible region
+        // codes. If the list contains more than one region, the first region for which the number is
+        // valid is returned.
+        private string GetRegionCodeForShortNumberFromRegionList(PhoneNumber number,
+            List<string> regionCodes)
+        {
+            if (regionCodes.Count == 0)
+            {
+                return null;
+            }
+
+            if (regionCodes.Count == 1)
+            {
+                return regionCodes.First();
+            }
+            string nationalNumber = GetNationalSignificantNumber(number);
+            foreach (string regionCode in regionCodes)
+            {
+                PhoneMetadata phoneMetadata = MetadataManager.GetShortNumberMetadataForRegion(regionCode);
+                if (phoneMetadata != null
+                    && MatchesPossibleNumberAndNationalNumber(nationalNumber, phoneMetadata.ShortCode))
+                {
+                    // The number is valid for this region.
+                    return regionCode;
+                }
+            }
+            return null;
         }
 
         /**
@@ -95,6 +265,38 @@ namespace PhoneNumbers
                 ? emergencyNumberPattern.MatchAll(normalizedNumber).Success
                 : emergencyNumberPattern.MatchBeginning(normalizedNumber).Success;
 
+        }
+
+        /**
+        * Gets the national significant number of the a phone number. Note a national significant number
+        * doesn't contain a national prefix or any formatting.
+        * <p>
+        * This is a temporary duplicate of the {@code getNationalSignificantNumber} method from
+        * {@code PhoneNumberUtil}. Ultimately a canonical static version should exist in a separate
+        * utility class (to prevent {@code ShortNumberInfo} needing to depend on PhoneNumberUtil).
+        *
+        * @param number  the phone number for which the national significant number is needed
+        * @return  the national significant number of the PhoneNumber object passed in
+        */
+        private static string GetNationalSignificantNumber(PhoneNumber number)
+        {
+            // If a leading zero has been set, we prefix this now. Note this is not a national prefix.
+            var nationalNumber = new StringBuilder(number.ItalianLeadingZero ? "0" : "");
+            nationalNumber.Append(number.NationalNumber);
+            return nationalNumber.ToString();
+        }
+
+        // TODO: Once we have benchmarked ShortNumberInfo, consider if it is worth keeping
+        // this performance optimization.
+        private bool MatchesPossibleNumberAndNationalNumber(String number,
+            PhoneNumberDesc numberDesc)
+        {
+            if (numberDesc.PossibleLengthCount > 0
+                && !numberDesc.PossibleLengthList.Contains(number.Length))
+            {
+                return false;
+            }
+            return matcherApi.matchNationalNumber(number, numberDesc, false);
         }
     }
 }
